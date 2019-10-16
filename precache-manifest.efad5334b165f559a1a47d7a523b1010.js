46719bdd0eac2101afa96904df632c63d7752ed2
Truong Nguyen <samuraitruong@hotmail.com>
2019-10-17 00:17:03 +1100

    Updates

---
+++ precache-manifest.efad5334b165f559a1a47d7a523b1010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646048ba08ac4be398116280ce613c2a",
+    "url": "/momenton-code-challenge/index.html"
+  },
+  {
+    "revision": "eea06e13dd4c78b90f4f",
+    "url": "/momenton-code-challenge/static/css/main.ae179a33.chunk.css"
+  },
+  {
+    "revision": "63999738bfe23f52c628",
+    "url": "/momenton-code-challenge/static/js/2.d4ad94f8.chunk.js"
+  },
+  {
+    "revision": "eea06e13dd4c78b90f4f",
+    "url": "/momenton-code-challenge/static/js/main.0129f96a.chunk.js"
+  },
+  {
+    "revision": "322f05bebc1904a28b05",
+    "url": "/momenton-code-challenge/static/js/runtime-main.51df7ada.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/momenton-code-challenge/static/media/logo.25bf045c.svg"
+  }
+]);
